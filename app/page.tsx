66aa3222b0116166066fd21f00df3c3682b981eb
mvm <root.mvm@gmail.com>
2025-10-28 15:39:00 -0500

    fix overflowing on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ export default async function Home() {
   const experiences = await getExperiences();
 
   return (
-    <div className="w-full grid grid-cols-auto-fit-600">
+    <div className="w-full grid xs:grid-cols-auto-fit-full sm:grid-cols-auto-fit-600">
       {experiences.map((experience) => (
         <Link
           href={`/editor/${experience.name}`}
           key={experience.name}
-          className="relative w-full h-96 transition-transform hover:scale-[1.01] hover:z-10"
+          className="relative w-full h-64 sm:h-80 md:h-96 transition-transform hover:scale-[1.01] hover:z-10"
         >
           <ExperienceProvider experience={experience} iframeScale={0.5}>
             <ExperienceIFrame className="pointer-events-none" />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,14 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "600px",
+      },
       gridTemplateColumns: {
         "auto-fit-300": "repeat(auto-fit, minmax(300px, 1fr))",
         "auto-fit-600": "repeat(auto-fit, minmax(600px, 1fr))",
+        "auto-fit-640": "repeat(auto-fit, minmax(640px, 1fr))",
+        "auto-fit-full": "repeat(auto-fit, minmax(0px, 1fr))",
       },
     },
   },
